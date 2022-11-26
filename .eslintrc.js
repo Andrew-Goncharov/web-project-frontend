065c8397c3df920f1b359676fe56e7ba53eec960
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-27 00:45:22 +0300

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+  ],
+  rules: {
+    indent: [
+      "error",
+      2,
+    ],
+    "linebreak-style": [
+      "error",
+      "unix",
+    ],
+    quotes: [
+      "error",
+      "double",
+    ],
+    semi: [
+      "error",
+      "always",
+    ],
+    "import/extensions": [
+      "warn",
+      "never",
+    ],
+    "import/no-unresolved": ["warn"],
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "no-param-reassign": ["warn"],
+  },
+};
