c67d437fad58e54d145eb25ba3e39d727872210d
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 21:59:21 +0300

    Added Not found page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NotFound.module.css";
+
+function NotFound() {
+  return <div>Page not found try something else</div>;
+}
+
+export default NotFound;
