ff423e0550a2fa25c14543e9364271e0c4cf588e
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 22:15:16 +0300

    Input component updated

--- src/components/UIKit/Input/Input.tsx
+++ src/components/UIKit/Input/Input.tsx
@@ -1,24 +1,36 @@
-import React, { ChangeEvent, KeyboardEventHandler } from "react";
+import React from "react";
 import styles from "./Input.module.css";
 
 export interface InputProps {
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  value: string | number;
-  className: string;
-  placeholder: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  value?: string | number;
+  className?: string;
+  placeholder?: string;
 }
 
+const defaultProps: InputProps = {
+  onChange: () => null,
+  onKeyDown: () => null,
+  value: "",
+  className: "",
+  placeholder: "Click here to start",
+};
+
 function Input({
-  onChange, value, className, placeholder,
+  onChange, value, className, placeholder, onKeyDown,
 }: InputProps) {
   return (
     <input
       className={`${styles.input} ${className}`}
       onChange={onChange}
+      onKeyDown={onKeyDown}
       value={value}
       placeholder={placeholder}
     />
   );
 }
+
+Input.defaultProps = defaultProps;
 
 export default Input;
