5783b450f5f50660759bdfde8198e15097b20413
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 22:01:39 +0300

    Added Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return <div className={styles.footerWrapper}>Footer</div>;
+}
+
+export default Footer;
