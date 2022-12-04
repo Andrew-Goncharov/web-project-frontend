dbe1fb5b829c449cdfb8fc8adae859f44536b2be
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 22:02:34 +0300

    App component updated to handle routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import Counter from "./app/features/counter/Counter";
+import { Route, Routes } from "react-router";
 import "./App.css";
+import { Footer } from "./components";
+import Header from "./components/Header/Header";
+import { Category, Main, Product } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit
-          {" "}
-          <code>src/App.tsx</code>
-          {" "}
-          and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,
-          <span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="applicationWrapper">
+      <Header />
+      <div className="appContentWrapper">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/category/:categoryId" element={<Category />} />
+          <Route path="*" element={<div>not found</div>} />
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
