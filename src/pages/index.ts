fdaf23038d0faf1956f9f10b53588df1e144043b
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 22:00:21 +0300

    Added pages index file

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+import Main from "./Main/Main";
+import Product from "./Product/Product";
+import Category from "./Category/Category";
+import NotFound from "./NotFound/NotFound";
+
+export {
+  Main, Product, Category, NotFound,
+};
