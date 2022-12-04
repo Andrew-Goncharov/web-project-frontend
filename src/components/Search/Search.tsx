706f0e9a4b27fd31ac9bd8ab7393e9b6377f4305
Иван Глебов <glebov.vanya8@gmail.com>
2022-12-04 22:15:24 +0300

    Search component updated

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../UIKit/Input/Input";
 import styles from "./Search.module.css";
 
 function Search() {
+  const [search, setSearch] = useState("");
+
   return (
     <div className={styles.searchWrapper}>
       <div className={styles.searcHeader}>Search</div>
       <div className={styles.searchInputWrapper}>
         <Input
           className={styles.searchInput}
-          onChange={(e) => console.log(e.target.value)}
-          value=""
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => console.log(e.key)}
+          value={search}
           placeholder="Your search here"
         />
       </div>
